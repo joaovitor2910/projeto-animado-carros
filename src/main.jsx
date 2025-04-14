00d38bab4af62d56e3ca53a21064f357e5a63d52
jv-dev007 <joaovitorsiq84@gmail.com>
2025-04-14 15:21:01 -0300

    modified home.jsx name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/home/home.jsx'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
